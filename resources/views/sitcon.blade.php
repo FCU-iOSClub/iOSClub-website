0efcb1112178250ec3341219b36f47f5ca4a5552
KUI\ahkui <ahkui@outlook.com>
2017-12-08 20:47:04 +0800

    update

--- resources/views/sitcon.blade.php
+++ resources/views/sitcon.blade.php
@@ -94,18 +94,6 @@
         </div>
     </div>
     <script>
-    function hideAddressBar() {
-      if (!window.location.hash) {
-        if (document.height < window.outerHeight) {
-          document.body.style.height = (window.outerHeight + 50) + 'px';
-        }
-
-        setTimeout(function() { window.scrollTo(0, 1); }, 50);
-      }
-    }
-
-    window.addEventListener("load", function() { if (!window.pageYOffset) { hideAddressBar(); } });
-    window.addEventListener("orientationchange", hideAddressBar);
     $(document).click(function(event) {
       var text = $(event.target).text();
     });
