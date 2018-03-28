4b0ec72fee3549683086d5cdce6cf1620a24e0de
KUI\ahkui <ahkui@outlook.com>
2018-03-28 23:06:09 +0800

    update route and composer

--- routes/web.php
+++ routes/web.php
@@ -17,37 +17,18 @@ Route::domain('sitcon.iosclub.tw')->group(function () {
     })->name('sitcon');
 });
 
-Route::domain('iosclub.tw')->group(function () {
-    Route::get('/', function () {
-        return view('home')->with(['title'=>'iosclub']);
-    })->name('home');
+Route::get('/', function () {
+    return view('home')->with(['title'=>'iosclub']);
+})->name('home');
 
-    Route::get('welcome', function () {
-        return view('welcome');
-    })->name('welcome');
+Route::get('welcome', function () {
+    return view('welcome');
+})->name('welcome');
 
-    Route::get('sitcon', function () {
-        return view('sitcon');
-    })->name('sitcon2');
-
-    Route::get('exam',function(){
-        return Redirect::to('https://docs.google.com/document/d/1QUeiozE6MYDrSKjNcBA8ty0Q8R5KEOax6bhEMrQbLIE/edit?usp=sharing');
-    });
-    Route::get('rule',function(){
-        return Redirect::to('https://docs.google.com/document/d/1jlszEJgLFNTw8dwNi_oapv_kzzQtfWShGmkzJfVciks/edit');
-    });
+Route::get('exam',function(){
+    return Redirect::to('https://docs.google.com/document/d/1QUeiozE6MYDrSKjNcBA8ty0Q8R5KEOax6bhEMrQbLIE/edit?usp=sharing');
+});
+Route::get('rule',function(){
+    return Redirect::to('https://docs.google.com/document/d/1jlszEJgLFNTw8dwNi_oapv_kzzQtfWShGmkzJfVciks/edit');
 });
 
-Route::domain('ip.ahkui.com')->group(function () {
-    Route::get('/', function () {
-        return view('home')->with(['title'=>'iosclub']);
-    });
-
-    Route::get('welcome', function () {
-        return view('welcome');
-    });
-
-    Route::get('sitcon', function () {
-        return view('sitcon');
-    });
-});
